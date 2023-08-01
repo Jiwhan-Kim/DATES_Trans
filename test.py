a407a0b60995727f00abdc10591d689dc80e5edf
KimJiWhan <jiwhan1015@yonsei.ac.kr>
2023-08-01 15:19:12 +0900

    23.08.01. Init

---
+++ test.py
@@ -0,0 +1,31 @@
+'''
+DATES Lab
+Transformer
+Model Check for CIFAR10
+Tseting
+Made by KimJW (SSE21)
+'''
+
+import torch
+import numpy as np
+from tqdm import tqdm
+from os import path
+
+import Models   as M
+import Trainers as T
+import Datas    as D
+
+# Set Devices (M1/M2 mps, NVIDIA cuda:0, else cpu)
+device = None
+if   torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda:0"
+else:
+    device = "cpu"
+
+# Global Data
+
+
+if __name__ == "__main__":
+    pass
